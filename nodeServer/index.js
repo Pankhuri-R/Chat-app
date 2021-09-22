9bef1e7aebe7bd37c97bad8d5de22d1fb15af927
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-09-22 23:20:30 +0530

    Init

---
+++ nodeServer/index.js
@@ -0,0 +1,19 @@
+//This is server side
+
+const io = require("socket.io")(8000);
+
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit("user-joined", username);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
