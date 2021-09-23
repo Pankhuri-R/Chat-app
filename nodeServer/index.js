b25f1893fa287960745ab803c5b76e3f3e4c7643
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-09-23 19:47:02 +0530

    Start of client side

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,11 +1,16 @@
 //This is server side
 
-const io = require("socket.io")(8000);
-
+const io = require("socket.io")(8000, {
+  cors: {
+    origin: "*",
+  },
+});
+console.log("Server up");
 const users = {};
 
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (username) => {
+    console.log("new user", username);
     users[socket.id] = username;
     socket.broadcast.emit("user-joined", username);
   });
